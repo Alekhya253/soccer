c76c70b0f0e1f761ce7e10eb896f85776525096c
Alekhya253 <20wh1a05b6@bvrithyderabad.edu.in>
2023-05-31 20:29:13 +0530

    adds playerlist

--- frontend/src/Components/Player/PlayerList.js
+++ frontend/src/Components/Player/PlayerList.js
@@ -1,8 +1,25 @@
 import React from 'react';
-//sfc - create a stateless function
 
-const PlayerList = () => {
-  return <div>PlayerList</div>;
+const PlayerList = (props) => {
+  return (
+    <div>
+      <ul className='collection with-header'>
+        <li className='collection-header'>
+          <h4>Players</h4>
+        </li>
+        {props.players.map((item) => (
+          <a
+            href='#!'
+            className='collection-item'
+            key={item._id}
+            onClick={() => props.updateCurrentPlayer(item)}
+          >
+            {item.firstName} {item.lastName}
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PlayerList;
